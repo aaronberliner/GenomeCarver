2719d2d6cf35cc534ad92d9129ac716864e4b387
t_berla <aaron.berliner@autodeskcom>
2014-05-16 17:08:29 -0700

    Berliner. Fixing more mistakes in path. Sigh.

--- cyborg/GenomeCarverPackage.js
+++ cyborg/GenomeCarverPackage.js
@@ -16,7 +16,7 @@ Autodesk.Cyborg.Applications.GenomeCarver = Autodesk.Cyborg.Applications.GenomeC
 
 console.log('Defining GenomeFactory');
 Autodesk.Cyborg.Applications.GenomeCarver.GenomeFactory = function (node){
-	var script = '/siteversion/cyborg/applications/GenomeCarver/cyborg/carveNode.py';
+	var script = '/siteversion/cyborg/applications/cai_lab/GenomeCarver/cyborg/carveNode.py';
     var content;
     adskrt.ajax({ 
         url: script, 
@@ -32,7 +32,7 @@ Autodesk.Cyborg.Applications.GenomeCarver.GenomeFactory = function (node){
 
 console.log('Defining FeaturesFactory');
 Autodesk.Cyborg.Applications.GenomeCarver.FeaturesFactory = function (node){
-	var script = '/siteversion/cyborg/applications/GenomeCarver/cyborg/GetSequenceNode.py';
+	var script = '/siteversion/cyborg/applications/cai_lab/GenomeCarver/cyborg/GetSequenceNode.py';
     var content;
     adskrt.ajax({ 
         url: script, 
@@ -49,7 +49,7 @@ Autodesk.Cyborg.Applications.GenomeCarver.FeaturesFactory = function (node){
 console.log('Defining carverFactory');
 Autodesk.Cyborg.Applications.GenomeCarver.carveFactory = function (node){
 	node.setNodeInfo('viewtype', 'process');
-	var script = '/siteversion/cyborg/applications/GenomeCarver/cyborg/CreatePrimerNode.py';
+	var script = '/siteversion/cyborg/applications/cai_lab/GenomeCarver/cyborg/CreatePrimerNode.py';
     var content;
     adskrt.ajax({ 
         url: script, 
